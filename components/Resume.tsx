1850df58af97ae5f6d62578cf0f387c6aacbeb07
Arthur Laroya <arthur.j.laroya@gmail.com>
2025-12-18 16:41:41 +1100

    feat: add Avatars component for skill icons; update Resume layout with expertise and skills sections

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -1,3 +1,4 @@
+import Avatar10 from "./ui/Avatars";
 import ResumeCard from "./ui/ResumeCard";
 import SectionHeader from "./ui/SectionHeader";
 
@@ -5,7 +6,7 @@ const Resume = () => {
   return (
     <section className="">
       <div className="grid grid-cols-3 border border-gray-300 min-h-[calc(100vh-60px)]">
-        <div className="border-r border-gray-300 p-10">
+        <div className="border-r border-gray-300 p-8">
           <SectionHeader title="Experience" />
           <div className="flex flex-col gap-5 mt-5">
             <ResumeCard />
@@ -13,10 +14,41 @@ const Resume = () => {
             <ResumeCard />
           </div>
         </div>
-        <div className="border-r border-gray-300 p-10">
-          <SectionHeader title="Expertise" />
+
+        <div className="border-r border-gray-300 p-8 flex flex-col gap-3">
+          <div>
+            <SectionHeader title="Expertise" />
+            <p className="pt-5 pb-2">
+              Logo, Branding, Packaging, Poster & Editorial, Social Media Post,
+              Motion Graphic
+            </p>
+            <div className="relative flex items-center justify-between w-full py-2">
+              <div className="w-1 h-1 bg-black rounded-full" />
+              <div className="flex-1 border-t border-black border-solid" />
+              <div className="w-1 h-1 bg-black rounded-full" />
+            </div>
+          </div>
+
+          <SectionHeader title="Hardskill" />
+          <div className="bg-black rounded-3xl p-3">
+            <div className="flex flex-col gap-3">
+              <img
+                src="/kizzy.webp"
+                alt="kizzy"
+                className="grayscale rounded-3xl max-h-35 object-cover"
+              />
+              {/* Skill Icons */}
+              <Avatar10 />
+            </div>
+          </div>
+          <div className="relative flex items-center justify-between w-full py-2">
+            <div className="w-1 h-1 bg-black rounded-full" />
+            <div className="flex-1 border-t border-black border-solid" />
+            <div className="w-1 h-1 bg-black rounded-full" />
+          </div>
+          <SectionHeader title="Softskill" />
         </div>
-        <div className="border-r border-gray-300 p-10">
+        <div className="border-r border-gray-300 p-8">
           <SectionHeader title="Education" />
         </div>
       </div>
---
+++ components/ui/Avatars.tsx
@@ -0,0 +1,55 @@
+export default function Avatar10() {
+  return (
+    <div className="flex items-end justify-center">
+      <div className="h-[50px] w-[50px] rounded-full border-2 border-white ">
+        <img
+          src="https://skillicons.dev/icons?i=ai"
+          alt="avatar"
+          className="h-full w-full rounded-full object-cover object-center"
+        />
+      </div>
+      <div className="-ml-4 h-[50px] w-[50px] rounded-full border-2 border-white ">
+        <img
+          src="https://skillicons.dev/icons?i=ps"
+          alt="avatar"
+          className="h-full w-full rounded-full object-cover object-center"
+        />
+      </div>
+      <div className="-ml-4 h-[50px] w-[50px] rounded-full border-2 border-white ">
+        <img
+          src="https://skillicons.dev/icons?i=ae"
+          alt="avatar"
+          className="h-full w-full rounded-full object-cover object-center"
+        />
+      </div>
+      <div className="-ml-4 h-[50px] w-[50px] rounded-full border-2 border-white ">
+        <img
+          src="https://skillicons.dev/icons?i=figma"
+          alt="avatar"
+          className="h-full w-full rounded-full object-cover object-center"
+        />
+      </div>
+      <div className="-ml-4 h-[50px] w-[50px] rounded-full border-2 border-white ">
+        <img
+          src="https://skillicons.dev/icons?i=instagram"
+          alt="avatar"
+          className="h-full w-full rounded-full object-cover object-center"
+        />
+      </div>
+      <div className="-ml-4 h-[50px] w-[50px] rounded-full border-2 border-white ">
+        <img
+          src="https://skillicons.dev/icons?i=html"
+          alt="avatar"
+          className="h-full w-full rounded-full object-cover object-center"
+        />
+      </div>
+      <div className="-ml-4 h-[50px] w-[50px] rounded-full border-2 border-white ">
+        <img
+          src="https://skillicons.dev/icons?i=css"
+          alt="avatar"
+          className="h-full w-full rounded-full object-cover object-center"
+        />
+      </div>
+    </div>
+  );
+}
